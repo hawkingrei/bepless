90666beca5fc940c963dfd40266e346aaac45c34
Weizhen Wang <wangweizhen@pingcap.com>
2026-04-20 22:56:04 +0800

    fix(proto): allow proto3 optional fields

--- worker/build.rs
+++ worker/build.rs
@@ -2,6 +2,7 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     tonic_build::configure()
         .build_client(false)
         .build_server(false)
+        .protoc_arg("--experimental_allow_proto3_optional")
         .compile_protos(
             &[
                 "proto/src/main/java/com/google/devtools/build/lib/buildeventstream/proto/build_event_stream.proto",
